ba689a0ae5dcc21c53e55bee3dd8c4cf5c73c910
“wcodes” <wang_mu@hotmail.com>
2015-03-10 03:14:10 +0000

    “Stukdo”

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sukdo::Application.config.secret_key_base = '9c2b223294964a6fe2afe5929b4c91785fb4af66d890afafd09544782d9d3ea9f0b0e07650a5b2b040b1e97917e3afc02e200bd80d1a62f2278dda916527cb55'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sukdo::Application.config.session_store :cookie_store, key: '_sukdo_session'
